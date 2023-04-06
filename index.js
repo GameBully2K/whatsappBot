c917925cbff502af2fe79e8dd02b410ed907cb7e
Bilal Mbarki <bilal.ftb2000@gmail.com>
2023-04-06 02:22:27 +0000

    final beta

--- index.js
+++ index.js
@@ -20,9 +20,14 @@ client.on('ready', () => {
 client.initialize();
 
 const listId = '900900099034';
+client.on ( 'message' , async message => {
+	if (message.body.match(/hokran/)){
+		client.sendMessage(message.from,'3afwan');
+	}
+});
 
 client.on ( 'message' , async message => {
-	if (message.body.match(/obot/)){
+	if (message.body == 'Robot' ||message.body == 'robot' ){
 		client.sendMessage(message.from,'مرحبا أنا روبوت تاع بلال عزل من الخيارات بارسال الرقم المناسب \n 1: إضافة مهمة \n 2: تفقد المهمات الخاصة بك\n 3: استحضار بلال ')
 	}
 });
@@ -31,7 +36,7 @@ client.on('message', async message => {
 	const contact = await message.getContact();
 	switch(message.body) {
 		case '1':	
-			client.sendMessage(message.from,'أدخل الرمز 11 متبوعا بفحوى المهمة');
+			client.sendMessage(message.from,'أدخل الرمز add task متبوعا بفحوى المهمة');
 			break;
 		case '2':
 			client.sendMessage(message.from, 'جاري التحميل');
@@ -39,7 +44,7 @@ client.on('message', async message => {
 				tags: [contact.pushname]
 			  }).toString();
 			const tasks = await  fetch(
-				`https://api.clickup.com/api/v2/list/${listId}/task?$${query}`,
+				`https://api.clickup.com/api/v2/list/${listId}/task?`,
 				{
 				  method: 'GET',
 				  headers: {
@@ -53,9 +58,12 @@ client.on('message', async message => {
 				let msgBody = 'طلباتك هي كالتالي: \n';
 				let num=0;
 				for (let i=data.tasks.length-1; i >= 0; i--) {
-					num++;
-					msgBody = msgBody + num.toString() + '- ' + data.tasks[i].name+ ' :: ' + data.tasks[i].status.status + '\n';
+					if (data.tasks[i].tags[0].name === contact.pushname.toLocaleLowerCase()) {
+						num++;
+						msgBody = msgBody + num.toString() + '- ' + data.tasks[i].name+ ' :: ' + data.tasks[i].status.status + '\n';
+					}
 				}
+				if (num == 0) msgBody = 'ليست لديك أي طلبات';
 				client.sendMessage(message.from, msgBody);
 			} else {
 				client.sendMessage('212687053026@c.us', 'khata2 f api mea '+ contact.pushname);
@@ -73,7 +81,7 @@ client.on('message', async message => {
 //list id 900900099034
 
 client.on('message', async message => {
-	if(message.body.match(/11/)) {
+	if(message.body.match(/^add task/i)) {
 		client.sendMessage(message.from, 'جاري اضافة المهمة');
 		const contact = await message.getContact();
 		const resp = await fetch(
@@ -85,7 +93,7 @@ client.on('message', async message => {
 				Authorization: 'pk_55506138_R7480Z7LXCAELLP1FAH47OKVC5RUZG23'
 			  },
 			  body: JSON.stringify({
-				name: message.body.slice(2),
+				name: message.body.slice(8),
 				tags: [contact.pushname]
 			  })
 			}
