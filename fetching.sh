74b6911fc5687018ba79fedaaab681e8d2b13198
Bilal Mbarki <bilal.ftb2000@gmail.com>
2023-04-08 05:49:23 +0000

    scripts

--- fetching.sh
+++ fetching.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-cp index.js /home/ubuntu/.cache/indexcp.js
+cp /home/ubuntu/dev/whatsappBot/index.js /home/ubuntu/.cache/indexcp.js
 
 copy=/home/ubuntu/.cache/indexcp.js
+cd /home/ubuntu/dev/whatsappBot
 git fetch
 git pull
-file=index.js
+file=/home/ubuntu/dev/whatsappBot/index.js
 
 if ! cmp -s "$file" "$copy"; then
-	
+	reboot
 fi
 
 rm /home/ubuntu/.cache/indexcp.js
