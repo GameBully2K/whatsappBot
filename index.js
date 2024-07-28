975de9a694806f26c4a59cc9dfc3f39de7932fb8
Bilal Mbarki <bilal.ftb2000@gmail.com>
2024-07-28 11:17:45 +0100

    fixed packages compatibility

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth(),
 	puppeteer: {
-		args: ['--no-sandbox']
+		args: ['--no-sandbox'],
 	},
     webVersionCache: {
         type: 'remote',
